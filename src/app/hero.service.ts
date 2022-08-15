baf0a8a862c45e4a1067e56bcef71ce72cd8c927
Jelena Cuka <cuka.jelena@gmail.com>
2022-08-15 13:30:45 +0200

    added input and button for saving new hero in hero component, added add method in hero component that calls heroService method for adding hero, added addHero method in heroService class for adding new hero with post http call

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,9 +16,17 @@ export class HeroService {
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
-    httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
